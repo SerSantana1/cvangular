24fd08886f2a350de5c60f31edb56279ba8a7a93
Srjnt_hm <santanaserjio@gmail.com>
2025-05-18 18:37:21 +0200

    Added favicon,changed site title and created fade in animation for every section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ScrollAnimationDirective } from '../scroll-animation.directive';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [ScrollAnimationDirective],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+animate = false;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.animate = true;
+    }, 100); // Delay to allow animation
+  }
 }
