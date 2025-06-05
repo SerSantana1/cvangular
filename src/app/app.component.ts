9cdf0de8fa1f63ed8b189119bfae657934c9158c
Srjnt_hm <santanaserjio@gmail.com>
2025-06-05 21:46:17 +0200

    added new cursor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,44 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContactComponent } from "./contact/contact.component";
-import { ResumeComponent } from "./resume/resume.component";
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 import { CookieService } from 'ngx-cookie-service';
 import { CookieManagerService } from '../services/cookie-manager.service';
 
-
-
-
 declare const window: any;
-
+declare const TweenMax: any;
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgbModule, HeaderComponent, HeroComponent, FooterComponent, ContactComponent, ResumeComponent],
+  imports: [
+    RouterOutlet,
+    NgbModule,
+    HeaderComponent,
+    HeroComponent,
+    FooterComponent,
+    ContactComponent,
+    ResumeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
-
   ngOnInit() {
     if (performance.navigation.type === 1) {
       window.location.href = window.location.origin;
     }
   }
 
-
-  constructor(private CookieManagerService: CookieManagerService) {}
+  constructor(private CookieManagerService: CookieManagerService) { }
 
   ngAfterViewInit(): void {
     window.cookieconsent.initialise({
       palette: {
         popup: { background: '#000' },
-        button: { background: '#f1d600' }
+        button: { background: '#f1d600' },
       },
       theme: 'classic',
       position: 'bottom-right',
@@ -52,7 +54,41 @@ export class AppComponent {
         } else if (status === 'dismiss') {
           this.CookieManagerService.rejectAllCookies();
         }
-      }
+      },
     });
+    const $bigBall = document.querySelector('.cursor__ball--big');
+    const $smallBall = document.querySelector('.cursor__ball--small');
+    const $hoverables = document.querySelectorAll('.hoverable');
+
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < $hoverables.length; i++) {
+      $hoverables[i].addEventListener('mouseenter', onMouseHover);
+      $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e: MouseEvent) {
+      TweenMax.to($bigBall, 0.4, {
+        x: e.clientX - 15,
+        y: e.clientY - 15
+      });
+      TweenMax.to($smallBall, 0.1, {
+        x: e.clientX - 5,
+        y: e.clientY - 7
+      });
+    }
+
+    // Hover an element
+    function onMouseHover() {
+      TweenMax.to($bigBall, 0.3, {
+        scale: 4,
+      });
+    }
+    function onMouseHoverOut() {
+      TweenMax.to($bigBall, 0.3, {
+        scale: 1,
+      });
+    }
   }
 }
