57533568e023af3dc0ce9f4cc2b2004755ff738e
SerSantana1 <santanaserjio01@gmail.com>
2025-10-19 23:27:09 +0200

    changed layout and new component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -3,11 +3,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../../environments/environment.development';
 import { ScrollAnimationDirective } from '../scroll-animation.directive';
 import {TranslatePipe, TranslateDirective, TranslateService} from "@ngx-translate/core";
+import { PortfolioComponent } from './resume-tabs/portfolio/portfolio.component';
 
 
 @Component({
   selector: 'app-resume',
-  imports: [NgbModule,ScrollAnimationDirective,TranslatePipe, TranslateDirective],
+  imports: [PortfolioComponent,NgbModule,ScrollAnimationDirective,TranslatePipe, TranslateDirective],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
